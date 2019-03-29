2c62a3151bc0b043c6d68b3d4deef6338201095e
yuripeixoto <yuripeixoto@gmail.com>
2019-03-29 16:13:11 -0400

    criada a rota do PageAdmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -15,6 +16,12 @@ $app->get('/', function() {
 
     $page->setTpl("index");
 
+});
+
+$app->get('/admin', function() {
+
+    $page = new PageAdmin();
+    $page->setTpl("index");
 });
 
 $app->run();
